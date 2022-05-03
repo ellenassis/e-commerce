a0d73e0ded4ed575a246cfd1eb85d5bfc67042b3
ellenassis <ellenassis8@gmail.com>
2022-05-03 15:37:39 -0300

    feat: add home

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Home } from "./pages/Home";
+
+import GlobalStyles from './styles/global';
+
+function App() {
+  return (
+   <div>
+     <Home />
+     <GlobalStyles />
+   </div>
+  );
+}
+
+export default App;
