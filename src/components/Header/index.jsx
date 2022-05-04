611dbafd7779d02b3e269223ed9f7c41d4a3e242
ellenassis <ellenassis8@gmail.com>
2022-05-04 14:49:55 -0300

    feat: add navbar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,45 @@
 import React from "react";
 
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
-import { Container, Wrapper, Left, Center, Right, Language } from "./styles";
+import {
+  Container,
+  Wrapper,
+  Left,
+  Center,
+  Right,
+  Language,
+  SearchContainer,
+  Input,
+  Logo,
+  MenuItem,
+} from "./styles";
+import { Badge } from "@mui/material";
 
 export const Header = () => {
   return (
     <Container>
       <Wrapper>
         <Left>
-            <Language>ENG</Language>
+          <Language>pt-br</Language>
+          <SearchContainer>
+            <Input placeholder="Pesquisar" />
+            <SearchIcon style={{color: "gray", fontSize: "20px"}} />
+          </SearchContainer>
         </Left>
-        <Center>TUDO BEM</Center>
-        <Right>TCHAU</Right>
+        <Center>
+          <Logo>MAGIC.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>Registrar</MenuItem>
+          <MenuItem>Entrar</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={2} color="secondary">
+             <ShoppingCartIcon />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -7,24 +7,57 @@ export const Container = styled.div`
 export const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
 `;
 
 export const Left = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+export const SearchContainer = styled.div `
+  border: 0.5px solid lightgray;  
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
 `;
 
 export const Language = styled.span `
-    font-size: 14px;
+    font-size: 16px;
     cursor: pointer;
+    text-transform: uppercase;
+`;
+
+export const Input = styled.input `
+  border: none;
 `;
 
 export const Center = styled.div`
   flex: 1;
 `;
 
+export const Logo = styled.h1 `
+  font-weight: bold;
+  text-align: center;
+  
+`;
+
 export const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  text-transform: uppercase;
 `;
+
+export const MenuItem = styled.div `
+  font-size: 16px;
+  cursor: pointer;
+  margin-left: 14px;
+`;
+
 
 
