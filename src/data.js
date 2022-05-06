c986462251c03dbd3a0a9e869cafc9a20eaaab97
ellenassis <ellenassis8@gmail.com>
2022-05-06 14:07:18 -0300

    feat: add data

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const slideData = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/NLZ5jgy/model2.png",
+    title: "Coleção de Outono",
+    description: "Ganhe 30% de desconto para novas chegadas.",
+    button: "Veja agora",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "Coleção de Verão",
+    description: "Ganhe 30% de desconto para novas chegadas.",
+    bg: "#256",
+    button: "Veja agora",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "Coleção de Inverno",
+    description: "Ganhe 30% de desconto para novas chegadas.",
+    button: "Veja agora",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "SHIRT STYLE!",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    title: "LIGHT JACKETS",
+  },
+];
