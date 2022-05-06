5333d899c86358287232e7fa37cea648a4b00da8
ellenassis <ellenassis8@gmail.com>
2022-05-06 17:21:45 -0300

    feat: add product component

---
+++ src/components/Products/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+
+import { Container, Circle, Image, Info, Icon } from './styles';
+
+export const Products = ({ item }) => {
+  return (
+    <Container>
+      <Circle />
+      <Image src={item.img} />
+      <Info>
+        <Icon>
+          <ShoppingCartOutlinedIcon />
+        </Icon>
+        <Icon>
+          <SearchOutlinedIcon />
+        </Icon>
+        <Icon>
+          <FavoriteBorderOutlinedIcon />
+        </Icon>
+      </Info>
+    </Container>
+  )
+}
