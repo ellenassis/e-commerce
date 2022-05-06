1bdedb6bfa931a6a981534b1a2cf96636e134574
ellenassis <ellenassis8@gmail.com>
2022-05-06 14:08:02 -0300

    feat: add  announcement

---
+++ src/components/Announcement/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Container } from './styles';
+
+export const Announcement = () => {
+  return (
+    <Container>
+        Super Oferta! Frete grátis para pedidos acima de R$100,00
+    </Container>
+  )
+}
