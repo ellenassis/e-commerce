d0a5f6be9195be1872a2c36a6d3c2b00a2b4669e
ellenassis <ellenassis8@gmail.com>
2022-05-06 14:08:46 -0300

    feat: hero start

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { slideData } from "../../data";
+
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+import {
+  Container,
+  Arrow,
+  Wrapper,
+  Slide,
+  ImgContainer,
+  Image,
+  InfoContainer,
+  Title,
+  Description,
+  Button,
+} from "./styles";
+
+export const Hero = () => {
+ const [slideIndex, setSlideIndex] = useState(0);
+
+ const HandleClick = direction => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0)
+    }
+ }
+
+  return (
+    <Container>
+      <Arrow direction="left" onClick={() => HandleClick("left")}>
+        <ArrowBackIosIcon />
+      </Arrow>
+      <Wrapper slideIndex={slideIndex}>
+        {slideData.map((item) => (
+          <Slide bg={item.bg}>
+            <ImgContainer>
+              <Image src={item.img} />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Description>
+                {item.description}
+              </Description>
+              <Button>{item.button}</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+    <Arrow direction="right" onClick={() => HandleClick("right")}>
+        <ArrowForwardIosIcon />
+      </Arrow>
+    </Container>
+  );
+};
---
+++ src/components/Hero/styles.js
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  position: relative;
+`;
+
+export const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: white;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  opacity: 0.5;
+  cursor: pointer;
+  z-index: 2;
+`;
+
+export const Wrapper = styled.div`
+  height: 100%;
+  margin-top: 50px;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${props => props.slideIndex * -100}vw);
+`;
+
+export const Slide = styled.div`
+  height: 100vh;
+  min-width: 100vw;
+  padding: 0 40px;
+  display: flex;
+  align-items: center;
+`;
+
+export const ImgContainer = styled.div`
+  height: 100%;
+  width: 50%;
+  flex: 1;
+`;
+
+export const Image = styled.img`
+  height: 80%;
+`;
+
+export const InfoContainer = styled.div`
+  text-transform: uppercase;
+  flex: 1;
+  padding: 50px;
+`;
+
+export const Title = styled.h1`
+  font-size: 70px;
+`;
+
+export const Description = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+
+export const Button = styled.button`
+  text-transform: uppercase;
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
