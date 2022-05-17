cad1f2e90efa83801ff1f039cd6152b5abd64c5a
ellenassis <ellenassis8@gmail.com>
2022-05-17 12:46:24 -0300

    feat: add product list page

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { Header } from '../components/Header'
+import { Announcement } from '../components/Announcement'
+import { SearchProducts } from '../components/SearchProducts'
+import { NewsLetter } from '../components/NewsLetter'
+import { Footer } from '../components/Footer'
+
+
+
+export const ProductList = () => {
+  return (
+    <>
+      <SearchProducts />
+    </>
+  )
+}
